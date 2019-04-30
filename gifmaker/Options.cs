5e1f555660ac7f77dbe7b4372f8e28ede134de13
Aaron Holmes <aaron@aaronholmes.net>
2019-04-30 16:08:45 -0700

    Add code to create a GIF from a sequence of JPGs. Include progress and command-line argument parsing.

---
+++ gifmaker/Options.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArgParser.Core;
+using ArgParser.Styles;
+using ArgParser.Styles.Extensions;
+
+namespace gifmaker
+{
+	public class Options
+	{
+		public static IParseResult Parse(string[] args)
+		{
+			return new ContextBuilder()
+			.AddParser<Options>("GIF Maker", help =>
+			{
+				help
+					.SetName("Gif Maker")
+					.SetShortDescription("Make a GIF from a sequence of images.");
+			})
+			.WithFactoryFunction(() => new Options())
+			.WithBooleanSwitch('h', "help", o => o.Help = true)
+			.WithSingleValueSwitch('i', "input", (o, s) => o.InputPath = s, help =>
+			 {
+				 help
+					 .SetName("Input Path")
+					 .SetShortDescription("The directory containing images to sequence into a GIF.");
+			 })
+			.WithSingleValueSwitch('o', "output", (o, s) => o.OutputPath = s, help =>
+			 {
+				 help
+					 .SetName("Input Path")
+					 .SetShortDescription("The directory containing images to sequence into a GIF.");
+			 })
+			.WithSingleValueSwitch('d', "delay", (o, s) => o.FrameDelay = int.Parse(s), help =>
+			{
+				help
+					.SetName("Frame Delay")
+					.SetShortDescription("The delay, in ms, between each frame of the GIF.");
+			})
+			.Finish
+			.RegisterExtensions()
+			.AddAutoHelp((parseResults, exceptions) =>
+			{
+				foreach (var kvp in parseResults)
+				{
+
+					var casted = kvp.Key as Options;
+					if (casted is null) continue;
+					if (casted.Help)
+						return kvp.Value.Id;
+				}
+
+				var missingValues = exceptions.OfType<MissingValueException>();
+				var first = missingValues.FirstOrDefault();
+				return first?.Parser.Id;
+			})
+			.Parse(args);
+		}
+		public string InputPath { get; set; }
+
+		private string _outputPath;
+		public string OutputPath
+		{
+			get
+			{
+				if (string.IsNullOrEmpty(_outputPath))
+				{
+					_outputPath = System.IO.Path.Combine(InputPath, "Output");
+				}
+				return _outputPath;
+			}
+			set { _outputPath = value; }
+		}
+
+		public int FrameDelay { get; set; }
+
+		public bool Help { get; set; }
+	}
+}
---
+++ gifmaker/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageMagick;
+
+namespace gifmaker
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Options options = null;
+			try
+			{
+				var parserResult = Options.Parse(args);
+
+				parserResult.When<Options>((o, p) => { options = o; });
+
+				if (options == null || options.Help)
+				{
+					Exit();
+				}
+
+				var progress = new Progress<GifWriterProgress>();
+				progress.ProgressChanged += (s,e) =>
+				{
+					var outputStr = $"{e.Text}: {e.ProgressPercentage}%";
+					var lineStr = new string(' ', Console.WindowWidth - outputStr.Length-1);
+					Console.Write($"\r{outputStr}{lineStr}");
+				};
+
+				var gifFilename = new GifWriter
+				{
+					InputDir = options.InputPath,
+					OutputDir = options.OutputPath,
+					FrameDelay = options.FrameDelay
+				}.Execute(progress);
+
+				Console.WriteLine($"\nFile written to \"{gifFilename}\"");
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.ToString());
+			}
+
+			Exit();
+		}
+
+		private static void Exit()
+		{
+			Console.Write("\nPress return to quit ...");
+			Console.ReadLine();
+
+			Environment.Exit(0);
+		}
+	}
+
+	public class GifWriterProgress
+	{
+		public int ProgressPercentage { get; set; }
+		public string Text { get; set; }
+	}
+
+	public class GifWriter
+	{
+		public string InputDir { get; set; }
+		public string OutputDir { get; set; }
+		public int FrameDelay { get; set; }
+
+		private int _totalProgressSteps;
+
+		public string Execute(IProgress<GifWriterProgress> progress = null)
+		{
+			if (!Directory.Exists(InputDir)) throw new Exception($"Directory \"{InputDir}\" does not exist.");
+
+			var files = Directory.EnumerateFiles(InputDir, "*.jpg", SearchOption.TopDirectoryOnly).ToArray();
+
+			if (!files.Any()) throw new Exception($"No files ending in \".jpg\" found in directory \"{InputDir}\"");
+
+			using (var collection = new MagickImageCollection())
+			{
+				_totalProgressSteps = files.Length + 4;
+				var steps = 0;
+				foreach (var file in files)
+				{
+					Report(progress, ++steps, "Add File");
+					collection.Add(file);
+					collection[0].AnimationDelay = FrameDelay;
+				}
+
+				QuantizeSettings settings = new QuantizeSettings
+				{
+					Colors = 256
+				};
+				Report(progress, ++steps, "Quantize");
+				collection.Quantize(settings);
+
+				Report(progress, ++steps, "Optimize");
+				collection.Optimize();
+
+				Report(progress, ++steps, "Create Dir");
+				Directory.CreateDirectory(OutputDir);
+
+				var filename = Path.Combine(OutputDir, $"out-{DateTime.UtcNow.ToString("yyyyMMddHHmmssfff")}.gif");
+
+				Report(progress, ++steps, "Write File");
+				collection.Write(filename);
+
+				return filename;
+			}
+		}
+
+		private void Report(IProgress<GifWriterProgress> progress, int step, string text)
+		{
+			if (progress == null) return;
+
+			var args = new GifWriterProgress
+			{
+				ProgressPercentage = 100 * step / _totalProgressSteps,
+				Text = text
+			};
+			progress.Report(args);
+		}
+	}
+}
